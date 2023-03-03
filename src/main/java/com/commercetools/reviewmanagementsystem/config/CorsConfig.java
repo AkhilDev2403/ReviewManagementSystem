40683dd74d4d4226828eab732b245dcd64f456f3
AkhilDev2003 <akhildevcse@gmail.com>
2023-03-03 11:41:08 +0530

    cors error

--- src/main/java/com/commercetools/reviewmanagementsystem/config/CorsConfig.java
+++ src/main/java/com/commercetools/reviewmanagementsystem/config/CorsConfig.java
@@ -4,15 +4,19 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
-public class WebSecurityConfig {
+public class CorsConfig {
     @Bean
     public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
+        return new WebMvcConfigurerAdapter() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("http://localhost:8080");
+                registry.addMapping("/reviews/**")
+                        .allowedOrigins("http://localhost:8080")
+                        .allowedMethods("GET", "POST", "DELETE", "PUT")
+                        .allowedHeaders("*");
             }
         };
     }
