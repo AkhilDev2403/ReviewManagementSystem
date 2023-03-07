86256402aa21af0e50a2cfe4b0b68c442a76ace7
AkhilDev2003 <akhildevcse@gmail.com>
2023-03-07 13:36:49 +0530

    cors error test

--- src/main/java/com/commercetools/reviewmanagementsystem/config/WebMvcConfig.java
+++ src/main/java/com/commercetools/reviewmanagementsystem/config/WebMvcConfig.java
@@ -9,6 +9,10 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedMethods("*").allowedHeaders("*").allowedOrigins("*");
+        registry.addMapping("/**").allowedMethods("*")
+                .allowedHeaders("Content-Type", "X-Requested-With", "accept", "Origin", "Access-Control-Request-Method",
+                        "Access-Control-Request-Headers")
+                .exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials")
+                .allowedOrigins("*");
     }
 }
