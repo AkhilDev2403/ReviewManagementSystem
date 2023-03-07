74261591d55db0b7b3c94fab58670d1c2827698a
AkhilDev2003 <akhildevcse@gmail.com>
2023-03-07 15:01:04 +0530

    cors error

--- src/main/java/com/commercetools/reviewmanagementsystem/ReviewManagementSystemApplication.java
+++ src/main/java/com/commercetools/reviewmanagementsystem/ReviewManagementSystemApplication.java
@@ -1,7 +1,9 @@
 package com.commercetools.reviewmanagementsystem;
 
+import com.commercetools.reviewmanagementsystem.config.CORSFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -11,6 +13,14 @@ public class ReviewManagementSystemApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ReviewManagementSystemApplication.class, args);
+	}
+	@Bean
+	public FilterRegistrationBean crosFilterRegistration(){
+		FilterRegistrationBean registrationBean = new FilterRegistrationBean(new CORSFilter());
+		registrationBean.setName("CORS Filter");
+		registrationBean.addUrlPatterns("/*");
+		registrationBean.setOrder(1);
+		return registrationBean;
 	}
 
 }
