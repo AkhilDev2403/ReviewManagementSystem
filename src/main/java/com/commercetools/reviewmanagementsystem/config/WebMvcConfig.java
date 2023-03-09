d3ff6afa86ef49acf2cb61f4712246f787419796
AkhilDev2003 <akhildevcse@gmail.com>
2023-03-09 12:25:44 +0530

    cors error3

--- src/main/java/com/commercetools/reviewmanagementsystem/config/WebMvcConfig.java
+++ src/main/java/com/commercetools/reviewmanagementsystem/config/WebMvcConfig.java
@@ -1,22 +1,21 @@
-//package com.commercetools.reviewmanagementsystem.config;
-//
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.servlet.config.annotation.CorsRegistry;
-//import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-//
-//@Configuration
-//@EnableWebMvc
-//public class WebMvcConfig implements WebMvcConfigurer {
-//
-//
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry) {
-//        registry.addMapping("/reviews/**")
-//                .allowedOrigins("*")
-//                .allowedMethods("PUT", "DELETE", "POST", "GET")
-//                .allowedHeaders("Access-Control-Allow-Headers", "token");
-//    }
-//
-//
-//}
+package com.commercetools.reviewmanagementsystem.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebMvcConfig implements WebMvcConfigurer {
+
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/reviews/**")
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .allowedHeaders("*");
+    }
+
+}
