9fa6bd2f1bda22b77fa8cd23f5d7341929f6b64c
AkhilDev2003 <akhildevcse@gmail.com>
2023-03-16 13:50:58 +0530

    cors error

--- src/main/java/com/commercetools/reviewmanagementsystem/model/response/ReviewResponse.java
+++ src/main/java/com/commercetools/reviewmanagementsystem/model/response/ReviewResponse.java
@@ -9,7 +9,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class ReviewResponse {
 
-    private String firstName;
 
     private String customerId;
 
--- src/main/java/com/commercetools/reviewmanagementsystem/service/ReviewService.java
+++ src/main/java/com/commercetools/reviewmanagementsystem/service/ReviewService.java
@@ -90,15 +90,12 @@ public class ReviewService {
         return finalAvgRating;
     }
 
-    public List<ReviewDto> getAllReview(Integer page, Integer size, String productId) {
+    public List<ReviewDto> getAllReview(Integer page, Integer size) {
         List<ReviewDto> returnValue = new ArrayList<>();
-        List<ReviewEntity> reviewEntityList = repository.findByProductId(productId);
-        if(reviewEntityList.isEmpty())
-            throw new CustomException(ErrorMessages.NO_REVIEWS.getErrorMessages());
         Pageable pageable = PageRequest.of(page, size);
         Page<ReviewEntity> reviewList = repository.findAll(pageable);
         List<ReviewEntity> entityList = reviewList.getContent();
-        for (ReviewEntity reviewEntity : reviewEntityList) {
+        for (ReviewEntity reviewEntity : entityList) {
             ReviewDto reviewDto = new ModelMapper().map(reviewEntity, ReviewDto.class);
             returnValue.add(reviewDto);
         }
